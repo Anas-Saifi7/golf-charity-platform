537cc449ca63bdc15cd31b3af29aac94a823f594
Anas <anas.saifi4201@gmail.com>
2026-04-02 13:01:23 +0530

    backend does not working than fix the backend file server.js

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,60 @@
+// import express from "express";
+// import dotenv from "dotenv";
+// dotenv.config();
+
+// import cors from "cors";
+// import connectDB from './config/db.js';
+
+
+// import authRoutes from "./routes/authRoutes.js";
+// import subscriptionRoutes from "./routes/subscriptionRoutes.js";
+// import scoreRoutes from "./routes/scoreRoutes.js";
+// import drawRoutes from "./routes/drawRoutes.js";
+// import charityRoutes from "./routes/charityRoutes.js";
+// import adminRoutes from "./routes/adminRoutes.js";
+// import userRoutes from "./routes/userRoutes.js";
+
+// const app = express();
+
+// app.use(cors({
+//   origin: function (origin, callback) {
+//     const allowedOrigins = process.env.CLIENT_URL.split(',');
+//     if (!origin || allowedOrigins.includes(origin)) {
+//       callback(null, true);
+//     } else {
+//       callback(new Error('Not allowed by CORS'));
+//     }
+//   },
+//   credentials: true
+// }));
+
+// connectDB();
+// // console.log(process.env.STRIPE_SECRET_KEY);
+
+// app.get("/", (req,res)=>{
+//   res.send("API Running");
+// });
+// app.use("/api/auth", authRoutes);
+// app.use("/api/subscription", subscriptionRoutes);
+// app.use("/api/scores", scoreRoutes);
+// app.use("/api/draw", drawRoutes);
+// app.use("/api/charity", charityRoutes)
+// app.use("/api/admin", adminRoutes);
+// app.use("/user", userRoutes);
+
+// const PORT = process.env.PORT || 5000;
+
+// app.listen(PORT, ()=>{
+//   console.log(`Server running on port ${PORT}`);
+// });
+
+
 import express from "express";
 import dotenv from "dotenv";
 dotenv.config();
 
 import cors from "cors";
 import connectDB from './config/db.js';
-
 
 import authRoutes from "./routes/authRoutes.js";
 import subscriptionRoutes from "./routes/subscriptionRoutes.js";
@@ -16,34 +66,31 @@ import userRoutes from "./routes/userRoutes.js";
 
 const app = express();
 
+// CORS sabse pehle
 app.use(cors({
-  origin: function (origin, callback) {
-    const allowedOrigins = process.env.CLIENT_URL.split(',');
-    if (!origin || allowedOrigins.includes(origin)) {
-      callback(null, true);
-    } else {
-      callback(new Error('Not allowed by CORS'));
-    }
-  },
-  credentials: true
+  origin: '*',
+  credentials: false
 }));
 
-connectDB();
-// console.log(process.env.STRIPE_SECRET_KEY);
+// Body parser
+app.use(express.json());
 
-app.get("/", (req,res)=>{
+connectDB();
+
+app.get("/", (req, res) => {
   res.send("API Running");
 });
+
 app.use("/api/auth", authRoutes);
 app.use("/api/subscription", subscriptionRoutes);
 app.use("/api/scores", scoreRoutes);
 app.use("/api/draw", drawRoutes);
-app.use("/api/charity", charityRoutes)
+app.use("/api/charity", charityRoutes);
 app.use("/api/admin", adminRoutes);
 app.use("/user", userRoutes);
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, ()=>{
+app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
